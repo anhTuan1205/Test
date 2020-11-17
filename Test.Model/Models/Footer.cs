07c7ab6d04b2c918b360d942bd632158aa2dc9ac
DESKTOP-6CLENTL\Anh tuan <anht7882@gmail.com>
2020-11-17 09:27:50 +0700

    the train add

---
+++ Test.Model/Models/Footer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string D { set; get; }
+
+        [Required]
+        public string Content { set; get; }
+    }
+}
